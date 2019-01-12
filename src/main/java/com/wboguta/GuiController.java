5c32e434c0ec4858316004beac03dfddee37ac24
heraklesik94 embrague <209175@student.pwr.edu.pl>
2019-01-12 16:42:09 +0100

    added semaphore to synchronize threads

--- src/main/java/com/wboguta/GuiController.java
+++ src/main/java/com/wboguta/GuiController.java
@@ -1,6 +1,8 @@
 package com.wboguta;
 
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.ScatterChart;
@@ -13,13 +15,16 @@ import org.mariuszgromada.math.mxparser.mXparser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.Semaphore;
+
 import static java.lang.System.out;
 
-public class GuiController
-{
+public class GuiController {
+
     private static final Logger log = LoggerFactory.getLogger(GuiController.class);
 
     @FXML private Button buttonRun;
+    @FXML private Button buttonStop;
     @FXML private ComboBox comboBoxFunction;
     @FXML private ScatterChart scatterChart;
     @FXML private TextField textFieldPopulationSize;
@@ -31,9 +36,14 @@ public class GuiController
     @FXML private CheckBox checkBoxMutation;
     @FXML private CheckBox checkBoxCrossingOver;
 
+    private Semaphore semaphore;
+    private boolean isStopped;
+
     @FXML
     public void initialize() {
         comboBoxFunction.getSelectionModel().selectFirst();
+        isStopped = true;
+        semaphore = new Semaphore(1); // Only one thread can access
     }
 
 
@@ -62,9 +72,24 @@ public class GuiController
 
 
     @FXML
+    void buttonStopClicked(ActionEvent event) {
+        isStopped = true;
+    }
+
+
+    @FXML
     void buttonRunClicked(ActionEvent event) {
 
         buttonRun.setDisable(true);
+        buttonStop.setDisable(false);
+        isStopped = false;
+        comboBoxFunction.setDisable(true);
+        textFieldGenotypeSize.setDisable(true);
+        textFieldGenotypeSize.setDisable(true);
+        textFieldIterations.setDisable(true);
+        textFieldRangeFrom.setDisable(true);
+        textFieldRangeTo.setDisable(true);
+        textFieldPopulationSize.setDisable(true);
 
         // parse function
         final Function function = new Function((String)comboBoxFunction.getValue());
@@ -78,6 +103,14 @@ public class GuiController
             alert.showAndWait();
 
             buttonRun.setDisable(false);
+            buttonRun.setDisable(false);
+            comboBoxFunction.setDisable(false);
+            textFieldGenotypeSize.setDisable(false);
+            textFieldGenotypeSize.setDisable(false);
+            textFieldIterations.setDisable(false);
+            textFieldRangeFrom.setDisable(false);
+            textFieldRangeTo.setDisable(false);
+            textFieldPopulationSize.setDisable(false);
             return;
         }
 
@@ -101,18 +134,29 @@ public class GuiController
         new Thread() {
 
             private void plotIndividuals(final int i) throws InterruptedException {
+
                 Platform.runLater(new Runnable() {
                     public void run() {
+
+                        try {
+                            semaphore.acquire(); // Acquire access
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
                         series.getData().clear();
                         scatterChart.getData().removeAll(series);
                         while (population.isNextIndividualXY()) {
                             double[] xy = population.getNextIndividualXY();
                             series.getData().add(new XYChart.Data(xy[0], xy[1]));
                         }
+
                         scatterChart.getData().addAll(series);
                         textFieldIterations.setText(String.valueOf(i));
+                        semaphore.release(); // Release lock
                     }
                 });
+
                 Thread.sleep(((Double) sliderSpeed.getValue()).intValue()/2);
             }
 
@@ -128,6 +172,7 @@ public class GuiController
                         // put population on a chart
                         plotIndividuals(i);
 
+                        semaphore.acquire(); // Acquire access
                         if (checkBoxMutation.isSelected()) {
                             population.mutation();
                         }
@@ -138,13 +183,33 @@ public class GuiController
 
                         population.calculateAdaptation(function);
                         population.calculateProbs();
+                        semaphore.release(); // Release lock
 
                         plotIndividuals(i);
 
+                        semaphore.acquire(); // Acquire access
                         population.pickNewPopulation();
+                        semaphore.release(); // Release lock
+
+                        if(isStopped == true) {
+                            break;
+                        }
                     }
 
-                    buttonRun.setDisable(false);
+                    Platform.runLater(new Runnable() {
+                        public void run() {
+                            buttonRun.setDisable(false);
+                            buttonStop.setDisable(true);
+                            comboBoxFunction.setDisable(false);
+                            textFieldGenotypeSize.setDisable(false);
+                            textFieldGenotypeSize.setDisable(false);
+                            textFieldIterations.setDisable(false);
+                            textFieldIterations.setText(String.valueOf(iterations));
+                            textFieldRangeFrom.setDisable(false);
+                            textFieldRangeTo.setDisable(false);
+                            textFieldPopulationSize.setDisable(false);
+                        }
+                    });
 
                 } catch (Exception e) {
                     e.printStackTrace();
